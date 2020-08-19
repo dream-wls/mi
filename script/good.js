73ed4dce24aa7404b9a3cb1be63eb8b915d34977
dream-wls <1620843302@qq.com>
2020-08-19 15:20:50 +0800

    8/19 15:20

--- script/good.js
+++ script/good.js
@@ -18,5 +18,145 @@ setInterval(() => {
     }
 }, 2000);
 
+
+var id = valueByName(location.search,"id");
+console.log(id);
+
 //数据渲染
-$.ajax()
+$.ajax({
+    type:'get',
+    url: '../data/goodsIteam.json',
+    dataType:'json',
+    success: function(json) {
+        console.log(json);
+        // 更改图片
+
+        // img-box
+        for(let i=0;i<json.length;i++){
+            if(json[i].id == id){
+                console.log(json[i]);
+                let myjson = json[i];
+                //判断选择的是哪个
+                var optionLis = $('.my-option-li .option-li');
+                // console.log(optionLis);
+                for(let j=0;j<optionLis.length;j++){
+                    // console.log(optionLis[j]);
+                    console.log($(optionLis[j]).hasClass('active'));
+                    if( $(optionLis[j]).hasClass('active')){
+                        // 然后我们进行数据的渲染
+                        // myjson[j];
+                        console.log(myjson[j].title);
+                        
+                        $('.product-con h2').text(myjson[j].title);
+                        $('.sale-desc').html(`
+                         <span class="sale-desc-red">
+                            ${myjson[j].tip1}
+                         </span>
+                         ${myjson[j].tip2}
+                        `);
+
+                        //更改右边图片
+                        var imgArrs = $('.img-box img');
+                        console.log(imgArrs);
+                        for(let n=0;n<imgArrs.length;n++){
+                            // imgArrs[n]
+                            console.log(imgArrs[n]);
+                            // imgUrl
+                            myjson[j].imgUrl;
+                            console.log(myjson[j].imgUrl);
+                            imgArrs[n].src = myjson[j].imgUrl[n];
+                        }
+                         
+
+                    }
+                }
+            }
+        }
+        
+        //更改标题
+        // $('h2').text('');
+        // $('.sale-desc-red').text('');
+        // $('.sale-desc').text('');
+
+    },
+    error: function() {
+        alert('请求失败');
+    }
+})
+
+//点击事件
+$('.my-option-li').on('click','.option-li',function(e){
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    console.log(e);
+
+
+    $.ajax({
+        type:'get',
+        url: '../data/goodsIteam.json',
+        dataType:'json',
+        success: function(json) {
+            console.log(json);
+            // 更改图片
+    
+            // img-box
+            for(let i=0;i<json.length;i++){
+                if(json[i].id == id){
+                    console.log(json[i]);
+                    let myjson = json[i];
+                    //判断选择的是哪个
+                    var optionLis = $('.my-option-li .option-li');
+                    // console.log(optionLis);
+                    for(let j=0;j<optionLis.length;j++){
+                        // console.log(optionLis[j]);
+                        console.log($(optionLis[j]).hasClass('active'));
+                        if( $(optionLis[j]).hasClass('active')){
+                            // 然后我们进行数据的渲染
+                            // myjson[j];
+                            console.log(myjson[j].title);
+                            
+                            $('.product-con h2').text(myjson[j].title);
+                            $('.sale-desc').html(`
+                             <span class="sale-desc-red">
+                                ${myjson[j].tip1}
+                             </span>
+                             ${myjson[j].tip2}
+                            `);
+    
+                           
+                             
+    
+                        }
+                    }
+                }
+            }
+            
+            //更改标题
+            // $('h2').text('');
+            // $('.sale-desc-red').text('');
+            // $('.sale-desc').text('');
+    
+        },
+        error: function() {
+            alert('请求失败');
+        }
+    })
+
+})
+
+
+function valueByName(search, name) {
+    var start = search.indexOf(name + "=");
+    if (start == -1) {
+        return null;
+    } else {
+        var end = search.indexOf("&", start);
+        if (end == -1) {
+            end = search.length;
+        }
+        //提取出想要键值对 name=value
+        var str = search.substring(start, end);
+        var arr = str.split("=");
+        return arr[1];
+    }
+}
